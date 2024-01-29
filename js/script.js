f2c4fa75ba8f219f5f4731df98b968f7b29af00d
Polvere243 <danielercoli@gmail.com>
2024-01-29 08:57:59 +0100

    finally completed milestone 1. Later better than never;)

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
+// controllo la connessione coll'html
 console.log("Vue Ok", Vue);
+
+// destrutturo gli oggetti che mi servono
+
+const {user, contacts} = data;
 
 const {createApp} = Vue;
 
 const app = createApp({
+    data () {
+        return{
+            user,
+            contacts
 
+        }
+    }
 })
 
 app.mount("#root");
