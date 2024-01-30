55ed43e22f9464f48868fb06a4eb642202455902
Polvere243 <danielercoli@gmail.com>
2024-01-30 19:53:12 +0100

    created filter

--- js/script.js
+++ js/script.js
@@ -11,9 +11,33 @@ const app = createApp({
     data () {
         return{
             user,
-            contacts
-
+            contacts,
+            activeId: 0,
+            searchText: ""
         }
+    },
+    computed: {
+        // per sapere sempre quale sia l'id attivo
+        isActiveID() {
+            contacts.forEach(contact => {
+                return activeID === this.contact.id;
+            })
+        },
+        filteredContacts() {
+            const searchTerm = this.searchText.toLowerCase();
+            return this.contacts.filter(contact => 
+            contact.name.toLowerCase().includes(searchTerm)
+            );
+                
+        }
+    
+    },
+    
+    methods:{
+        setActiveId(id) {
+            id = this.contacts.id;
+            console.log(this.contacts.id)
+        } 
     }
 })
 
