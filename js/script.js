d350cb5f8b6af7ee06aead76a55b3c6314b99279
Polvere243 <danielercoli@gmail.com>
2024-03-22 20:59:48 +0100

    failed trying to fix the computed to find the contacts' id

--- js/script.js
+++ js/script.js
@@ -3,42 +3,66 @@ console.log("Vue Ok", Vue);
 
 // destrutturo gli oggetti che mi servono
 
-const {user, contacts} = data;
+const { user, contacts } = data;
 
-const {createApp} = Vue;
+const { messages } = contacts;
+console.log(messages);
+
+// monto l'app Vue 
+const { createApp } = Vue;
 
 const app = createApp({
-    data () {
-        return{
+    data() {
+        return {
             user,
             contacts,
-            activeId: 0,
-            searchText: ""
+            activeId: null,
+            searchText: "",
+            newText: "",
         }
+
+
     },
     computed: {
-        // per sapere sempre quale sia l'id attivo
-        isActiveID() {
-            contacts.forEach(contact => {
-                return activeID === this.contact.id;
-            })
-        },
+
+        // per creare il filtro di ricerca tra i contatti
         filteredContacts() {
             const searchTerm = this.searchText.toLowerCase();
-            return this.contacts.filter(contact => 
-            contact.name.toLowerCase().includes(searchTerm)
+            return this.contacts.filter(contact =>
+                contact.name.toLowerCase().includes(searchTerm)
             );
-                
-        }
-    
+
+        },
+        // per sapere sempre quale sia l'id attivo
+        isActiveId() {
+            return this.activeId = this.Contacts.find(({ id }) => id === contacts.id)
+
+        },
+
     },
-    
-    methods:{
+
+    methods: {
         setActiveId(id) {
-            id = this.contacts.id;
-            console.log(this.contacts.id)
-        } 
+            console.log(this.messages);
+        },
+        // metodo per aggiungere un nuovo messaggio
+        createNewMessage() {
+            if (!this.newText) return;
+            const newMessage = {
+                id: new Date().toISOString(),
+                status: "sent",
+                text: this.newText,
+            };
+            contacts.messages.push(newMessage);
+
+        }
+
+    },
+    created() {
+        return this.activeId === contacts[0].id;
     }
-})
+}
+
+)
 
 app.mount("#root");
